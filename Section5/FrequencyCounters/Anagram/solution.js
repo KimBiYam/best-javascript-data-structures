a0f042a86836054de76c61774d07757c3d3c8660
ChangHyunKim <hot9998@naver.com>
2022-07-07 01:38:36 +0900

    Rename solution files

--- Section5/FrequencyCounters/Anagram/solution.js
+++ Section5/FrequencyCounters/Anagram/solution.js
@@ -8,9 +8,9 @@ function validAnagram(first, second) {
   for (let i = 0; i < first.length; i++) {
     let letter = first[i];
     // if letter exists, increment, otherwise set to 1
-    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
   }
-  console.log(lookup)
+  console.log(lookup);
 
   for (let i = 0; i < second.length; i++) {
     let letter = second[i];
@@ -26,4 +26,4 @@ function validAnagram(first, second) {
 }
 
 // {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
-validAnagram('anagrams', 'nagaramm')
+validAnagram("anagrams", "nagaramm");
