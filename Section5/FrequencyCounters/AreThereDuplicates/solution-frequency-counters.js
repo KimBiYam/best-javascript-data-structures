98d5576d9670c7f9d7beca3121dd4f8c01bddf01
ChangHyunKim <hot9998@naver.com>
2022-07-07 00:42:59 +0900

    Add solutions for example problems

---
+++ Section5/FrequencyCounters/AreThereDuplicates/solution-frequency-counters.js
@@ -0,0 +1,10 @@
+function areThereDuplicates() {
+  let collection = {};
+  for (let val in arguments) {
+    collection[arguments[val]] = (collection[arguments[val]] || 0) + 1;
+  }
+  for (let key in collection) {
+    if (collection[key] > 1) return true;
+  }
+  return false;
+}
