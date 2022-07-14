db2f91741f8bce866a2341eea8fdd1b949e659b4
ChangHyunKim <hot9998@naver.com>
2022-07-15 00:21:01 +0900

    Solve `binarySearch` exercise

---
+++ Section10/BinarySearchExercise/mySolution.js
@@ -0,0 +1,17 @@
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+    if (arr[middle] === target) {
+      return middle;
+    } else if (arr[middle] < target) {
+      left = middle + 1;
+    } else if (arr[middle] > target) {
+      right = middle - 1;
+    }
+  }
+
+  return -1;
+}
