87fcba79247e23b08dc33340dfabec3c9b40304b
ChangHyunKim <hot9998@naver.com>
2022-07-17 12:56:53 +0900

    Add `JavaScriptBuiltInSort` examples

---
+++ Section11/JavaScriptBuiltInSort/numberCompare.js
@@ -0,0 +1,6 @@
+function numberCompare(num1, num2) {
+  return num1 - num2;
+}
+
+[6, 4, 15, 10].sort(numberCompare);
+// [4, 6, 10, 15]
---
+++ Section11/JavaScriptBuiltInSort/sortString.js
@@ -0,0 +1,3 @@
+["steele", "colt", "Data Structures", "Algorithms"].sort();
+// ["Algorithms", "Colt", "Data Structures", "Steele"]
+// 문자는 자동적으로 알파벳 순으로 정렬하게 되어있다.
