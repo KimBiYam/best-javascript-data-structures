134533fd07725525ffadc02843efb7d57627c634
ChangHyunKim <hot9998@naver.com>
2022-07-17 13:15:41 +0900

    Solve `BubbleSort` exmaple

---
+++ Section11/BubbleSort/mySolution.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  for (let i = arr.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([5, 1, 1, 2, 0, 0]));
