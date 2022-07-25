c2a89be5ce20e28c248377a1d6d498adc3607221
ChangHyunKim <hot9998@naver.com>
2022-07-25 23:16:47 +0900

    Add solution for `SinglyLinkList: pop method`

--- Section19/SinglyLinkedList/solution.js
+++ Section19/SinglyLinkedList/solution.js
@@ -23,8 +23,26 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+  pop() {
+    if (!this.head) return undefined;
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
 var list = new SinglyLinkedList();
-// list.push("HELLO")
-// list.push("GOODBYE")
+list.push("HELLO");
+list.push("GOODBYE");
+list.push("!");
