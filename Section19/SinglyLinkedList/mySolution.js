0742f376cf02b8d2c0df6fc3b084aaa7edcd621f
ChangHyunKim <hot9998@naver.com>
2022-07-26 02:30:12 +0900

    Solve `SinglyLinkedList: get method`

--- Section19/SinglyLinkedList/mySolution.js
+++ Section19/SinglyLinkedList/mySolution.js
@@ -77,6 +77,18 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+
+    let current = this.head;
+
+    for (let i = 0; i < index; i++) {
+      current = current.next;
+    }
+
+    return current;
+  }
 }
 
 const list = new SinglyLinkedList();
@@ -84,6 +96,4 @@ list.push("HELLO");
 list.push("GOODBYE");
 list.push("!");
 
-list.unshift("NEW");
-
-console.log(list);
+console.log(list.get(2));
