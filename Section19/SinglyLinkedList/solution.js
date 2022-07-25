ba02ace727c5692054e76cb0f1ee3ac3ea488075
ChangHyunKim <hot9998@naver.com>
2022-07-26 03:19:35 +0900

    Add solution for `SinglyLinkedList: insert method`

--- Section19/SinglyLinkedList/solution.js
+++ Section19/SinglyLinkedList/solution.js
@@ -72,6 +72,19 @@ class SinglyLinkedList {
     }
     return current;
   }
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
+
+    var newNode = new Node(val);
+    var prev = this.get(index - 1);
+    var temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
+  }
 }
 
 var list = new SinglyLinkedList();
