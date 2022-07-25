0d7f1fad36ed97cecf7deaa712e71840896a1fe6
ChangHyunKim <hot9998@naver.com>
2022-07-25 23:50:17 +0900

    Add solution for `SinglyLinkedList: shift method`

--- Section19/SinglyLinkedList/solution.js
+++ Section19/SinglyLinkedList/solution.js
@@ -40,6 +40,16 @@ class SinglyLinkedList {
     }
     return current;
   }
+  shift() {
+    if (!this.head) return undefined;
+    var currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
 }
 
 var list = new SinglyLinkedList();
