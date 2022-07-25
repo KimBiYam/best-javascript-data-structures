e660f583007941b44e98735eb2919f93827294f1
ChangHyunKim <hot9998@naver.com>
2022-07-25 23:09:22 +0900

    Solve `SinglyLinkedList: pop method`

--- Section19/SinglyLinkedList/mySolution.js
+++ Section19/SinglyLinkedList/mySolution.js
@@ -14,7 +14,7 @@ class SinglyLinkedList {
 
   push(val) {
     const newNode = new Node(val);
-    if (this.head === null) {
+    if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
     } else {
@@ -25,8 +25,37 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+  }
 }
 
 const list = new SinglyLinkedList();
 list.push("HELLO");
 list.push("GOODBYE");
+list.push("!");
+
+list.pop();
+list.pop();
+list.pop();
+console.log(list);
