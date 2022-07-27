371df9454c9a39e89ab4d5c93841e6dedc4b9488
ChangHyunKim <hot9998@naver.com>
2022-07-27 15:18:42 +0900

    Add solution for `DoublyLinkedList: push method`

--- Section20/DoublyLinkedList/solution.js
+++ Section20/DoublyLinkedList/solution.js
@@ -12,4 +12,17 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    var newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
