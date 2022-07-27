03e0b07d96bc7a85f612323434382448bba65352
ChangHyunKim <hot9998@naver.com>
2022-07-27 15:08:54 +0900

    Add skeleton for `DoublyLinkedList`

---
+++ Section20/DoublyLinkedList/mySolution.js
@@ -0,0 +1,15 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
