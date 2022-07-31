fb583bc0eb90b0f7ba0c3e037c74505d41056a41
ChangHyunKim <hot9998@naver.com>
2022-08-01 01:00:40 +0900

    Add stack array example

---
+++ Section21/Stack/stack-arrayExample.js
@@ -0,0 +1,9 @@
+// 배열의 기본 메소드인 push와 pop을 이용하면 스택처럼 사용이 가능하다.
+// 이를 가지고 클래스를 만들어서 구현 할 수도 있다.
+const stack = [];
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.pop();
+stack.pop();
+stack.pop();
