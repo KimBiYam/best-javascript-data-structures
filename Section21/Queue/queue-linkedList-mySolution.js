7b13d64e61fc706144b7ee4ee5ddef94d7fdb2f4
ChangHyunKim <hot9998@naver.com>
2022-08-17 01:01:34 +0900

    Solve `Queue:linkedList dequeue method`

--- Section21/Queue/queue-linkedList-mySolution.js
+++ Section21/Queue/queue-linkedList-mySolution.js
@@ -25,4 +25,29 @@ class Queue {
 
     return ++this.size;
   }
+
+  dequeue() {
+    if (this.size === 0) return null;
+
+    const nodeToDequeue = this.first;
+
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+    }
+
+    this.size--;
+    return nodeToDequeue.value;
+  }
 }
+
+const queue = new Queue();
+console.log(queue.enqueue(1));
+console.log(queue.enqueue(2));
+console.log(queue.enqueue(3));
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
