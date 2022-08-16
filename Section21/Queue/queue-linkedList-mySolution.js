13f93670e40800bf832483f89cb0d2750ebefa1c
ChangHyunKim <hot9998@naver.com>
2022-08-17 00:58:35 +0900

    Solve `Queue:linkedList enqueue method`

---
+++ Section21/Queue/queue-linkedList-mySolution.js
@@ -0,0 +1,28 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+
+    return ++this.size;
+  }
+}
