1b6ffaf49ae29cc30aa21f15e4600c54e9f1128f
ChangHyunKim <hot9998@naver.com>
2022-08-24 00:27:24 +0900

    Solve `BinarySearchTree: insert method`

--- Section22/mySolution.js
+++ Section22/mySolution.js
@@ -10,10 +10,42 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value > current.value) {
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      } else {
+        if (!current.left) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      }
+    }
+  }
 }
 
 const tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left.right = new Node(9);
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
+console.log(tree.root);
