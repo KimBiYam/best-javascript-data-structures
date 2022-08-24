5e582121c973678c7f8197cd6d14f1370b958da5
ChangHyunKim <hot9998@naver.com>
2022-08-25 00:48:39 +0900

    Add solution for `BinarySearchTree: find, contains methods`

--- Section22/solution.js
+++ Section22/solution.js
@@ -34,6 +34,37 @@ class BinarySearchTree {
       }
     }
   }
+  find(value) {
+    if (this.root === null) return false;
+    var current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return undefined;
+    return current;
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    var current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
 }
 
 //      10
