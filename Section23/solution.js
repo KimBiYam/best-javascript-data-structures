a0fdcd07894aeceab5825fb4ad417a9f940a5d9f
ChangHyunKim <hot9998@naver.com>
2022-08-29 00:08:34 +0900

    Add solution for `DFS:PostOrder`

--- Section23/solution.js
+++ Section23/solution.js
@@ -89,6 +89,16 @@ class BinarySearchTree {
     traverse(this.root);
     return data;
   }
+  DFSPostOrder() {
+    var data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
 }
 
 var tree = new BinarySearchTree();
