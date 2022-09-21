1f87ad8550afda45e0de0e0f07242382f0a8fc64
Chang Hyun Kim <ckdvk9998@gmail.com>
2022-09-22 01:09:36 +0900

    Add solution for `HashTable: keys, values methods`

--- Section25/solution.js
+++ Section25/solution.js
@@ -31,6 +31,32 @@ class HashTable {
     }
     return undefined;
   }
+  keys() {
+    let keysArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!keysArr.includes(this.keyMap[i][j][0])) {
+            keysArr.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return keysArr;
+  }
+  values() {
+    let valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!valuesArr.includes(this.keyMap[i][j][1])) {
+            valuesArr.push(this.keyMap[i][j][1]);
+          }
+        }
+      }
+    }
+    return valuesArr;
+  }
 }
 
 let ht = new HashTable(17);
@@ -41,3 +67,9 @@ ht.set("salmon", "#FA8072");
 ht.set("lightcoral", "#F08080");
 ht.set("mediumvioletred", "#C71585");
 ht.set("plum", "#DDA0DD");
+ht.set("purple", "#DDA0DD");
+ht.set("violet", "#DDA0DD");
+
+ht.keys().forEach(function (key) {
+  console.log(ht.get(key));
+});
