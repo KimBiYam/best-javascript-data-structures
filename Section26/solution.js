35d5fd6bd8899fec923dc76bfccba47706b7351e
Chang Hyun Kim <ckdvk9998@gmail.com>
2022-09-26 00:44:05 +0900

    Add solution for `Graph: addEdge method`

--- Section26/solution.js
+++ Section26/solution.js
@@ -5,4 +5,13 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
 }
+
+let g = new Graph();
+g.addVertex("Dallas");
+g.addVertex("Tokyo");
+g.addVertex("Aspen");
