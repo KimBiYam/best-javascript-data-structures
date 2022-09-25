9d6429236da093e2c88caff26fef936495df104a
Chang Hyun Kim <ckdvk9998@gmail.com>
2022-09-26 00:43:19 +0900

    Solve `Graph: addEdge method`

--- Section26/mySolution.js
+++ Section26/mySolution.js
@@ -8,9 +8,16 @@ class Graph {
       this.adjacencyList[vertex] = [];
     }
   }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
 }
 
 const graph = new Graph();
 graph.addVertex("hello");
+graph.addVertex("bye");
+graph.addEdge("hello", "bye");
 
 console.log(graph.adjacencyList);
