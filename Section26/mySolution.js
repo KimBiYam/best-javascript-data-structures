e1feb54c23995621d6e189213012295cf4de5164
Chang Hyun Kim <ckdvk9998@gmail.com>
2022-09-26 00:49:37 +0900

    Solve `Graph: removeEdge method`

--- Section26/mySolution.js
+++ Section26/mySolution.js
@@ -13,6 +13,15 @@ class Graph {
     this.adjacencyList[vertex1].push(vertex2);
     this.adjacencyList[vertex2].push(vertex1);
   }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (vertex) => vertex !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (vertex) => vertex !== vertex1
+    );
+  }
 }
 
 const graph = new Graph();
@@ -20,4 +29,6 @@ graph.addVertex("hello");
 graph.addVertex("bye");
 graph.addEdge("hello", "bye");
 
+console.log(graph.adjacencyList);
+console.log(graph.removeEdge("bye", "hello"));
 console.log(graph.adjacencyList);
