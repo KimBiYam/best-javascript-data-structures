f2f03103c59bdce799cd30ae7f685d98b253a0d0
Chang Hyun Kim <ckdvk9998@gmail.com>
2022-10-03 23:02:59 +0900

    Implement `WeightedGraph` class

---
+++ Section28/mySolution.js
@@ -0,0 +1,24 @@
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1, vertex2, weight) {
+    this.adjacencyList[vertex1].push({ node: vertex2, weight });
+    this.adjacencyList[vertex2].push({ node: vertex1, weight });
+  }
+}
+
+const graph = new WeightedGraph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addEdge("A", "B", 9);
+graph.addEdge("A", "C", 5);
+graph.addEdge("B", "C", 7);
+
+console.log(graph.adjacencyList);
