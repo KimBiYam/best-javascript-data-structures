8562fbed47386d96b57b3c307dfbab18ccca9790
Chang Hyun Kim <ckdvk9998@gmail.com>
2022-10-05 00:51:59 +0900

    Add solution fibonacci with memoization

--- Section29/fibonacci.js
+++ Section29/fibonacci.js
@@ -1,4 +1,16 @@
-function fib(num) {
+// function fib(num) {
+//   if (num <= 2) return 1;
+//   return fib(num - 1) + fib(num - 2);
+// }
+
+// memoization을 활용한 피보나치 수열 해결법
+function fib(num, memo = {}) {
+  if (memo[num]) return memo[num];
   if (num <= 2) return 1;
-  return fib(num - 1) + fib(num - 2);
+
+  const result = fib(num - 1, memo) + fib(num - 2, memo);
+  memo[num] = result;
+  return result;
 }
+
+console.log(fib(100));
