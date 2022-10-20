e1c42e70ead2fc989244d0bc323522f44e7daa99
Chang Hyun Kim <ckdvk9998@gmail.com>
2022-10-21 01:54:45 +0900

    Solve `Queues-enqueue` practice problem

---
+++ PracticeProblems/Queues-enqueue.js
@@ -0,0 +1,37 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+
+    this.size++;
+    return this.size;
+  }
+}
+
+var queue = new Queue();
+queue.enqueue(10); // 1
+queue.size; // 1
+queue.enqueue(100); // 2
+queue.size; // 2
+queue.enqueue(1000); // 3
+queue.size; // 3
